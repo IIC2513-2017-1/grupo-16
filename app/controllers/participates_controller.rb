609e9e2494bfb6433ad2d934a3e0aa9f119dd94c
Diego <difernandez@uc.cl>
2017-04-30 18:23:28 -0300

    Added participate button, without previously participating check

---
+++ app/controllers/participates_controller.rb
@@ -0,0 +1,36 @@
+class ParticipatesController < ApplicationController
+ 
+  # GET /participates/1
+  # GET /participates/1.json
+  def show
+  end
+
+  # GET /participates/new
+  def new
+    @participate = Participate.new
+    @raffle = Raffle.find(params[:raffle])
+  end
+
+  # POST /participates
+  # POST /participates.json
+  def create
+    @participate = Participate.new(participate_params)
+
+    respond_to do |format|
+      if @participate.save
+        format.html { redirect_to @participate, notice: 'participate was successfully created.' }
+        format.json { render :show, status: :created, location: @participate }
+      else
+        format.html { render :new }
+        format.json { render json: @participate.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def participate_params
+      params.require(:participate).permit(:user_id, :raffle_id, :raffle)
+    end
+end
---
+++ app/views/participates/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "participates/participate", participate: @participate
