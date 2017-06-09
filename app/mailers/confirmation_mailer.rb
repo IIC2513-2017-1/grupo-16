3fef368293fe4d52f03641b1c09500cbee791232
difernandez <difernandez@uc.cl>
2017-06-09 11:33:54 -0400

    Added number confirmed to confirmed_email sent.

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,9 +1,11 @@
 class ConfirmationMailer < ApplicationMailer
   default from: "rifapp@example.com"
 
-  def confirmed_email(user, creator)
-    @user = user
-    @creator = creator
-    mail(to: @user.email, subject: 'Confirmación de pago')
+  def confirmed_email(participate)
+    @user = User.find(participate.user_id)
+    @raffle = Raffle.find(participate.raffle_id)
+    @creator = User.find(@raffle.user_id)
+    @number = participate.number
+    mail(to: "difernandez@uc.cl", subject: 'Confirmación de pago')
   end
 end
