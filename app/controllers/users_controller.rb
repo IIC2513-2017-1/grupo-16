7b3947970c439cac3e9e62193c46bba7a17bb061
difernandez <difernandez@uc.cl>
2017-06-26 03:49:26 -0400

    Added token upon user creation via app, and show token on user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         UserMailer.new_user_email(@user).deliver!
+        @user.generate_token_and_save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
